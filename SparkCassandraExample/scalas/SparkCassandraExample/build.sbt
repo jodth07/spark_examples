94c846d3a60d07a74b0adba36e483e8587f5d935
jodth07 <dimanche.nav@gmail.com>
2020-05-08 11:06:50 -0400

    5/8 - cassandra - added jupyter notebook codes

---
+++ SparkCassandraExample/scalas/SparkCassandraExample/build.sbt
@@ -0,0 +1,7 @@
+name := "SparkCassandraExample"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+
---
+++ SparkCassandraExample/scalas/SparkCassandraExample/src/main/scala/com/dimanche/examples/SparkCassandraExample.scala
@@ -0,0 +1,12 @@
+package com.dimanche.examples
+
+
+object SparkCassandraExample {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World")
+
+  }
+
+}
