be414d9a5fed6ef6ece41cbbbf4035ee59a822fe
jodth07 <dimanche.nav@gmail.com>
2020-05-10 14:41:41 -0400

    5/10 - cassandra - added pyspark cassandra

--- SparkCassandraExample/scalas/SparkCassandraExample/src/main/scala/com/dimanche/examples/SparkCassandraExample.scala
+++ SparkCassandraExample/scalas/SparkCassandraExample/src/main/scala/com/dimanche/examples/SparkCassandraExample.scala
@@ -1,10 +1,28 @@
 package com.dimanche.examples
+import com.datastax.spark.connector._
+import org.apache.spark.sql.cassandra._
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 
 object SparkCassandraExample {
 
   def main(args: Array[String]): Unit = {
 
+    val logger = Logger.getLogger("org")
+    logger.setLevel(Level.ERROR)
+
+    val conf = new SparkConf().setAppName("Cassandra Example").setMaster("local[*]")
+
+    val spark  = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    
     println("Hello World")
 
   }
