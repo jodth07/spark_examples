eedb9f16533662fc67e25e5153b2991c84ef6d1a
jodth07 <dimanche.nav@gmail.com>
2020-05-11 14:32:45 -0400

    5/11 - hbase - added base scala code

--- SparkHBaseExample/scalas/SparkHbaseBatchExample/src/main/scala/com/dimanche/examples/SparkHbaseBatchExample.scala
+++ SparkHBaseExample/scalas/SparkHbaseBatchExample/src/main/scala/com/dimanche/examples/SparkHbaseBatchExample.scala
@@ -1,14 +1,9 @@
 package com.dimanche.examples
-import java.nio.file.Paths
-import org.apache.hadoop.hbase.protobuf.generated.TableProtos.TableName
-import org.apache.spark.sql.SparkSession
+
 import org.apache.log4j.{Level, Logger}
-//import org.apache.hadoop.hbase.spark.datasources.HBaseTableCatalog
+import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.hbase.spark.datasources.HBaseTableCatalog
 
-//import org.apache.hadoop.
-import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Put}
-
-import org.apache.hadoop.hbase.util.ByteStringer
 
 object SparkHbaseBatchExample {
 
@@ -16,7 +11,7 @@ object SparkHbaseBatchExample {
   val master : String = "local"
 
   def main(args: Array[String]): Unit = {
-
+    //  Spark and logger  Configurations
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     val spark : SparkSession = SparkSession.
@@ -24,24 +19,38 @@ object SparkHbaseBatchExample {
       appName(appName).
       master(master).
       getOrCreate()
+    import spark.implicits._
 
-//    val HbaseConf =
+    case class Employee(key: String, fName: String, lName: String,
+                        mName:String, addressLine:String, city:String,
+                        state:String, zipCode:String)
 
-    val currentPath = Paths.get(System.getProperty("user.dir"))
-    val baseBath = currentPath.getParent().getParent().getParent()
-    val genusesPath = Paths.get(baseBath.toString, "_inputdata", "genuses.json")
+    val data = Seq(Employee("1","Abby","Smith","K","3456 main", "Orlando","FL","45235"),
+      Employee("2","Amaya","Williams","L","123 Orange","Newark", "NJ", "27656"),
+      Employee("3","Alchemy","Davis","P","Warners","Sanjose","CA", "34789"))
 
-    val genusesDF = spark.read.json(s"file://${genusesPath}")
-    genusesDF.printSchema()
+    val df = spark.sparkContext.parallelize(data).toDF
 
-    val tableName = "genuses"
-//    val table = TableName.valueOf(tableName)
-    val ddl : String = "create 'genuses' 'main', 'class', 'division', 'family', 'kingdom', 'order', 'subkingdom'"
+    def catalog =
+      s"""{
+         |"table":{"namespace":"default", "name":"employee"},
+         |"rowkey":"key",
+         |"columns":{
+         |"key":{"cf":"rowkey", "col":"key", "type":"string"},
+         |"fName":{"cf":"person", "col":"firstName", "type":"string"},
+         |"lName":{"cf":"person", "col":"lastName", "type":"string"},
+         |"mName":{"cf":"person", "col":"middleName", "type":"string"},
+         |"addressLine":{"cf":"address", "col":"addressLine", "type":"string"},
+         |"city":{"cf":"address", "col":"city", "type":"string"},
+         |"state":{"cf":"address", "col":"state", "type":"string"},
+         |"zipCode":{"cf":"address", "col":"zipCode", "type":"string"}
+         |}
+         |}""".stripMargin
 
-//  Table genuses
-//  Column families : main, class, division, family, kingdom, name, order, subkingdom
-//  Key id
-
+    df.write.options(
+      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "4"))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .save()
 
   }
 }
