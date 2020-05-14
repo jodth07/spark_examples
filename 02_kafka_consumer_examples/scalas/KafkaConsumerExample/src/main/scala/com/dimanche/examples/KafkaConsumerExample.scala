514d5d5b7c55c1041745afa50a44e199f5f2833d
jodth07 <dimanche.nav@gmail.com>
2020-05-14 00:25:43 -0400

    5/14 - kafka | consumer - updated python & scala consumers

--- 02_kafka_consumer_examples/scalas/KafkaConsumerExample/src/main/scala/com/dimanche/examples/KafkaConsumerExample.scala
+++ 02_kafka_consumer_examples/scalas/KafkaConsumerExample/src/main/scala/com/dimanche/examples/KafkaConsumerExample.scala
@@ -1,8 +1,5 @@
 package com.dimanche.examples
 
-
-import org.apache.log4j.{Level, Logger}
-
 import java.util.Properties
 import java.util.Collections.singletonList
 
@@ -11,32 +8,29 @@ import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
 
 object KafkaConsumerExample {
 
-  val TOPIC : String  = "topic_one"
+    val TOPIC : String  = "topic_one"
 
-  val props : Properties = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put ("auto.offset.reset", "latest" )
-  props.put ("group.id", "topic_one" )
-  props.put("key.deserializer", new StringDeserializer().getClass.getName)
-  props.put("value.deserializer", new StringDeserializer().getClass.getName)
+    val props : Properties = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put ("auto.offset.reset", "latest" )
+    props.put ("group.id", "topic_one" )
+    props.put("key.deserializer", new StringDeserializer().getClass.getName)
+    props.put("value.deserializer", new StringDeserializer().getClass.getName)
 
 
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-    val kafkaConsumer = new KafkaConsumer[String, String](props)
+        val kafkaConsumer = new KafkaConsumer[String, String](props)
+        kafkaConsumer.subscribe(singletonList(TOPIC))
 
-    kafkaConsumer.subscribe(singletonList(TOPIC))
+        while(true){
+            val records : ConsumerRecords[String,String] = kafkaConsumer.poll(2000)// poll is currently deprecated
+            val recordsIter = records.iterator()
 
-    while(true){
-
-      val records : ConsumerRecords[String,String] = kafkaConsumer.poll(2000)// poll is currently deprecated
-      val recordsIter = records.iterator()
-
-      while (recordsIter.hasNext){
-        val record = recordsIter.next
-        println(s"${record.offset()} \t ${record.value()}")
-      }
-
+            while (recordsIter.hasNext){
+                val record = recordsIter.next
+                println(s"${record.offset()} \t ${record.value()}")
+            }
+        }
     }
-  }
 }
--- 02_kafka_consumer_examples/scalas/KafkaTwitterConsumerExample/src/main/scala/com/dimanche/examples/KafkaTwitterConsumerExample.scala
+++ 02_kafka_consumer_examples/scalas/KafkaTwitterConsumerExample/src/main/scala/com/dimanche/examples/KafkaTwitterConsumerExample.scala
@@ -11,41 +11,40 @@ import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
 
 object KafkaTwitterConsumerExample {
 
-  val TOPIC : String  = "topic_one"
+    val TOPIC : String  = "topic_one"
 
-  val props : Properties = new Properties()
-  props.put("bootstrap.servers", "localhost:9092")
-  props.put ("auto.offset.reset", "earliest" )
-  props.put ("group.id", "topic_one" )
-  props.put("key.deserializer", new StringDeserializer().getClass.getName)
-  props.put("value.deserializer", new StringDeserializer().getClass.getName)
+    val props : Properties = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put ("auto.offset.reset", "earliest" )
+    props.put ("group.id", "topic_one" )
+    props.put("key.deserializer", new StringDeserializer().getClass.getName)
+    props.put("value.deserializer", new StringDeserializer().getClass.getName)
 
 
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-    val kafkaConsumer = new KafkaConsumer[String, String](props)
-    kafkaConsumer.subscribe(singletonList(TOPIC))
+        val kafkaConsumer = new KafkaConsumer[String, String](props)
+        kafkaConsumer.subscribe(singletonList(TOPIC))
 
-    while(true){
+        while(true){
 
-      val records : ConsumerRecords[String,String] = kafkaConsumer.poll(2000)// poll is currently deprecated
-      val recordsIter = records.iterator()
+            val records : ConsumerRecords[String,String] = kafkaConsumer.poll(2000)// poll is currently deprecated
+            val recordsIter = records.iterator()
 
-      while (recordsIter.hasNext){
-        val record = recordsIter.next
-        val currentOffset = record.offset()
-        println(currentOffset)
-        try {
-            val jsRecord = record.value().parseJson.asJsObject()
-            val data = (jsRecord.getFields("user"),jsRecord.getFields("lang"),
-              jsRecord.getFields("id"),jsRecord.getFields("text"),
-              jsRecord.getFields("created_at"))
-            println(currentOffset, data)
-        } finally {
-            println("finally")
+            while (recordsIter.hasNext){
+                val record = recordsIter.next
+                val currentOffset = record.offset()
+                println(currentOffset)
+                try {
+                    val jsRecord = record.value().parseJson.asJsObject()
+                    val data = (jsRecord.getFields("user"),jsRecord.getFields("lang"),
+                      jsRecord.getFields("id"),jsRecord.getFields("text"),
+                      jsRecord.getFields("created_at"))
+                    println(currentOffset, data)
+                } finally {
+                    println("finally")
+                }
+            }
         }
-
-      }
     }
-  }
 }
