eb6055893b0f04a293d4f212dd6d42e79adfc25b
jodth07 <dimanche.nav@gmail.com>
2020-06-04 07:27:26 -0400

    6/4 - kafka - added constants to java producer

--- 01_kafka_producer_example/java/KafkaProducerExampleTwitter/src/main/java/com/dimanche/examples/KafkaTwitterProducerExample.java
+++ 01_kafka_producer_example/java/KafkaProducerExampleTwitter/src/main/java/com/dimanche/examples/KafkaTwitterProducerExample.java
@@ -5,17 +5,18 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
-
 public class KafkaTwitterProducerExample {
 
     static KafkaProducer<String, String> getProducer (){
         Properties props = new Properties();
-        props.put("bootstrap.servers", "10.0.0.9:9092");
+        props.put("bootstrap.servers", Constants.BOOTSTRAP_SERVER);
         props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
+        props.put("retries", Constants.RETRIES);
+        props.put("batch.size", Constants.BATCH_SIZE);
         props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
+        props.put("buffer.memory", Constants.BUFFER_MEMORY);
+        props.put("group.id", Constants.GROUP_ID); // default topic name
+        props.put("enable.auto.commit", Constants.AUTO_COMMIT);
         props.put("key.serializer",   StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
 
