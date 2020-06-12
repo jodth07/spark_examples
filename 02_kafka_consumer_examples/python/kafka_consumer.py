467ed088d8f4f620903797750429f8022505bb81
jodth07 <dimanche.nav@gmail.com>
2020-06-12 14:00:19 -0400

    6/12 - kafka python - updated code based on pylint

--- 02_kafka_consumer_examples/python/kafka_consumer.py
+++ 02_kafka_consumer_examples/python/kafka_consumer.py
@@ -1,18 +1,24 @@
-from kafka import KafkaConsumer
+"""
+@auth jodth07
+"""
+
 from datetime import datetime
+
+from kafka import KafkaConsumer
 
 
 if __name__ == '__main__':
 
     TOPIC = "topic_one"
-    fmt = "%Y-%m-%d %H:%M:%S"
+    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+    BOOTSTRAP_SERVERS = ["localhost:9092"]
 
     consumer = KafkaConsumer(TOPIC,
-                             bootstrap_servers=["localhost:9092"],
-                             group_id="counters"
+                             bootstrap_servers=BOOTSTRAP_SERVERS,
+                             group_id="twitter-topic_one"
                              )
 
-    for record in consumer:
-        print(record.value,
-              record.offset,
-              datetime.fromtimestamp(record.timestamp / 1000.).strftime(fmt))
+    for tweet_record in consumer:
+        print(tweet_record.value,
+              tweet_record.offset,
+              datetime.fromtimestamp(tweet_record.timestamp / 1000.).strftime(DATE_FORMAT))
--- 02_kafka_consumer_examples/python/kafka_twitter_consumer.py
+++ 02_kafka_consumer_examples/python/kafka_twitter_consumer.py
@@ -1,3 +1,8 @@
+"""
+this is an example of receiving twitter streams.
+@auth jodth07
+"""
+
 import json
 from pprint import pprint
 from kafka import KafkaConsumer
@@ -5,10 +10,11 @@ from kafka import KafkaConsumer
 
 if __name__ == '__main__':
 
+    BOOTSTRAP_SERVERS = ["localhost:9092"]
     TOPIC = "topic_one"
 
     consumer = KafkaConsumer(TOPIC,
-                             bootstrap_servers=["localhost:9092"],
+                             bootstrap_servers=BOOTSTRAP_SERVERS,
                              group_id="counters"
                              )
 
